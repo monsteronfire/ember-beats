2f6230ff2341ca74be51ba1668ded0b7e474da7a
monsteronfire <monsteronfire@gmail.com>
2016-12-26 16:53:57 +0800

    Display channel data in song template

--- app/routes/song.js
+++ app/routes/song.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 import Song from 'ember-beats/models/song';
 
 export default Ember.Route.extend({
+  queryParams: {
+    data: {
+      refreshModel: true
+    }
+  },
+
   model(params) {
     //console.log('PARAMS', params);
     return Song.fromEncodedBase64(params.data);
