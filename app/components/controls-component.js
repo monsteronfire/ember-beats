78ae34ce2c38deaba9012499ef72aef30f2dc023
monsteronfire <monsteronfire@gmail.com>
2017-01-19 08:00:17 +0800

    Add next function to increment tick count

--- app/components/controls-component.js
+++ app/components/controls-component.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  playbackService: Ember.inject.service()
+  playbackService: Ember.inject.service(),
+
+  actions: {
+    next() {
+      this.get('playbackService').next();
+    }
+  }
 });
--- app/services/playback-service.js
+++ app/services/playback-service.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   tickCount: 0,
 
+  next() {
+    this.incrementProperty('tickCount');
+  },
+
   display: Ember.computed('bars', 'beats', 'sixteenths', function () {
     let bars = this.get('bars');
     let beats = this.get('beats');
